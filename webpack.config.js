cb422aa21ef8ec39991d719173883fc21784cab6
鹿鸣 <>
2019-07-01 10:08:58 +0800

    add: 1. html模版 2. 动态插入js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
   devServer: {
     contentBase: './dist'
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+       template: './public/index.html',
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
